0e77c5d5ee75a765dbf55cb69555c892d999ee6b
Ratchapol Gunhompoo <ratchapol99pathway@gmail.com>
2024-06-20 22:30:11 +0700

    first commit

--- src/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -70,7 +70,7 @@ import UserProfile from '@/layouts/components/UserProfile.vue'
         <!-- eslint-enable -->
 
         <h1 class="font-weight-medium leading-normal text-xl text-uppercase">
-          Materio
+          My web App
         </h1>
       </RouterLink>
 
